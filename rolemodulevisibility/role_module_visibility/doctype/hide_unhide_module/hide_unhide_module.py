4f3ffcc7471d5faa45f94180b3d777fa6ceebb3f
jvfiel <bailabph@gmail.com>
2016-11-27 10:05:31 +0800

    added functionality

--- rolemodulevisibility/role_module_visibility/doctype/hide_unhide_module/hide_unhide_module.py
+++ rolemodulevisibility/role_module_visibility/doctype/hide_unhide_module/hide_unhide_module.py
@@ -5,6 +5,21 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.desk.doctype.desktop_icon.desktop_icon import set_hidden
 
-class HideUnhideModule(Document):
-	pass
+class RoleModuleVisibility(Document):
+	def autoname(self):
+		if self.role:
+			self.name = self.role
+	def validate(self):
+		#self.name = self.role
+		for user in frappe.db.sql("""SELECT parent FROM `tabUserRole` WHERE role='{0}'""".format(self.role)):
+			for module in self.modules:
+				print module.module
+				set_hidden(module.module, user=user[0], hidden=self.hidden)
+		if self.is_global:
+			for module in self.modules:
+				hideGlobal(module.module,self.hidden)
+
+def hideGlobal(module,hidden):
+	set_hidden(module, user=None, hidden=hidden)
